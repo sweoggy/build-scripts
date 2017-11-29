3be1e07f91ec06e8e3323a3297eae2bf64363693
Oscar Reimer <oscarreimer93@hotmail.com>
2017-11-29 16:35:29 +0100

    Update php-zts.sh
    - Added missing m4 dependency
    - Fixed pthreads commit reset
    - Changed echo to printf for more compatible print

--- php-zts.sh
+++ php-zts.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-apt install libreadline-dev libenchant-dev libxml2-dev libpcre3-dev libbz2-dev libcurl4-openssl-dev libjpeg-dev libpng12-0 libxpm-dev libfreetype6-dev libmysqlclient-dev libgd-dev libgmp-dev libsasl2-dev libmhash-dev unixodbc-dev freetds-dev libpspell-dev libsnmp-dev libtidy-dev libxslt1-dev libmcrypt-dev
+apt install m4 libreadline-dev libenchant-dev libxml2-dev libpcre3-dev libbz2-dev libcurl4-openssl-dev libjpeg-dev libpng12-0 libxpm-dev libfreetype6-dev libmysqlclient-dev libgd-dev libgmp-dev libsasl2-dev libmhash-dev unixodbc-dev freetds-dev libpspell-dev libsnmp-dev libtidy-dev libxslt1-dev libmcrypt-dev
 
 wget http://launchpadlibrarian.net/140087283/libbison-dev_2.7.1.dfsg-1_amd64.deb
 wget http://launchpadlibrarian.net/140087282/bison_2.7.1.dfsg-1_amd64.deb
@@ -10,10 +10,11 @@ dpkg -i bison_2.7.1.dfsg-1_amd64.deb
 git clone https://github.com/php/php-src.git -b PHP-7.1 --depth=1
 cd php-src/ext
 git clone https://github.com/krakjoe/pthreads -b master pthreads
+cd pthreads
 git reset --hard 527286336ffcf5fffb285f1bfeb100bb8bf5ec32
-cd ..
+cd ../../
 
-echo "\n\nBuilding and installing PHP\n\n"
+printf "\n\nBuilding and installing PHP\n\n"
 
 ./buildconf --force
 
@@ -41,7 +42,7 @@ ln -s -f /opt/php-zts/bin/pear /usr/bin/pear-zts
 ln -s -f /opt/php-zts/bin/pecl /usr/bin/pecl-zts
 
 
-echo "\n\nBuilding and installing pthreads\n\n"
+printf "\n\nBuilding and installing pthreads\n\n"
 
 cd ext/pthreads*
 phpize-zts
@@ -51,7 +52,7 @@ phpize-zts
 make && make install
 
 
-echo "\n\nInstalling various extensions\n\n"
+printf "\n\nInstalling various extensions\n\n"
 
 cd ../../
 
